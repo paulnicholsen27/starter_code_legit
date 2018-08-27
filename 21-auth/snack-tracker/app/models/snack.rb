5fce639e9e66380068210a327e658260512b0445
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-08-27 13:58:03 -0400

    add .rb

--- 21-auth/snack-tracker/app/models/snack.rb
+++
@@ -1,8 +0,0 @@
-class Snack < ApplicationRecord
-    belongs_to :retailer, optional: true
-
-    validates :name, presence: true
-    validates :calories, presence: true
-    validates :deliciousness, presence: true
-
-end
