22f7eff0efe48b4b83f7cd3d16fc0855dd19f5df
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-01-28 10:35:08 -0500

    cleanup before starting

--- 28-js-events/script.js
+++ 28-js-events/script.js
@@ -1,6 +1,6 @@
-console.log("first line")
-setTimeout(function(){console.log("hello right away")}, 1000)
-console.log("hello from bottom of the file")
+// console.log("first line")
+// setTimeout(function(){console.log("hello right away")}, 1000)
+// console.log("hello from bottom of the file")
 
 
 //document.addEventListener("DOMContentLoaded", function(){
