567ba7a392b12577ade2bfdfe8eafe7849b52733
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-01-28 10:33:25 -0500

    preplanning 28

---
+++ 28-js-events/script.js
@@ -0,0 +1,8 @@
+console.log("first line")
+setTimeout(function(){console.log("hello right away")}, 1000)
+console.log("hello from bottom of the file")
+
+
+//document.addEventListener("DOMContentLoaded", function(){
+//
+//})
