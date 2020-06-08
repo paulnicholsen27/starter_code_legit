4cd407db475f1f0e289b64e4e1c9e2c07689a5d0
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-06-08 10:06:51 -0400

    changed m2m

--- 04-one-to-many-starter/run.rb
+++ 04-one-to-many-starter/run.rb
@@ -1,7 +1,2 @@
-require_relative "tweet.rb"
-require_relative "user.rb"
-puts "hi"
-coffee_dad = User.new("Coffee Dad")
-
-puts coffee_dad.is_instance? User 
-puts coffee_dad.username == "Coffee Dad"
+require_relative "./school.rb"
+require_relative "./instructor.rb"
--- 05-many-to-many-starter/do_now.rb
+++ 05-many-to-many-starter/do_now.rb
@@ -1,25 +1,22 @@
+# Build out these classes and methods so that a restaurant can track the meals it serves, 
+# and a Meal knows all the restaurants where it is served.  e.g., if McDonalds and Burger King
+# both serve hamburgers and chicken sandwiches, `mcdonalds.meals` would include both hamburgers
+# and chicken sandwiches, and `hamburger.restaurants` would include both McDonalds and Burger King
+
 class Restaurant
 
     def initialize(name)
-        @name = name
-        @meals = []
     end
 
     def add_meal(meal)
-        self.meals << meal
-        meal.restaurants << self
     end
 
 class Meal
 
     def initialize(name)
-        @name = name
-        @restaurants = []
     end
 
     def add_restaurant(restaurant)
-        self.restaurants << restaurant
-        restaurant.meals << self
     end
 
 end
